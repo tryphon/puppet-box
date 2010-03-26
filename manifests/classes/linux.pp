cf2b67d4661bd1373b71d3da2f0ec1ec9c7512a5
Florent Peyraud <florent@tryphon.eu>
2010-03-26 18:32:42 +0100

    changed kernel to 2.6.32

--- manifests/classes/linux.pp
+++ manifests/classes/linux.pp
@@ -1,7 +1,7 @@
 class linux::kernel-2-6-30 {
   include apt::backport
   package { "linux-image-2.6-686":
-    ensure => "2.6.30+20~bpo50+1",
+    ensure => "2.6.32+25~bpo50+1",
     require => Apt::Source["lenny-backports"]
   }
 	package { "linux-image-2.6.26-2-686":
