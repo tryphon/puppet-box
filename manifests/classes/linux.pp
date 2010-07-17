b1f75d2e67a436164b044cd46e9d8114c2936067
Alban Peignier <alban.peignier@free.fr>
2010-07-17 23:21:21 +0200

    Update linux kernel release to 2.6.32+27~bpo50+1

--- manifests/classes/linux.pp
+++ manifests/classes/linux.pp
@@ -1,7 +1,7 @@
 class linux::kernel-2-6-30 {
   include apt::backport
   package { "linux-image-2.6-686":
-    ensure => "2.6.32+25~bpo50+1",
+    ensure => "2.6.32+27~bpo50+1",
     require => Apt::Source::Pin["linux-image-2.6-686"]
   }
 	package { "linux-image-2.6.26-2-686":
