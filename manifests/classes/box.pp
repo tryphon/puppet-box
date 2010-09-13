c6614bce9b94bdf8b94967356abd4d93adb293e7
Alban Peignier <alban@tryphon.eu>
2010-09-13 20:50:13 +0200

    Create box classes. Refs #5

---
+++ manifests/classes/box.pp
@@ -0,0 +1,39 @@
+class box {
+  include network
+  include network::interfaces
+
+  include network
+  include network::interfaces
+
+  include linux::kernel-2-6-30
+  include syslog
+  include smtp
+  include nano
+  include ssh
+  
+  include dbus::readonly
+  include avahi
+  
+  include apt
+  include apt::tryphon
+  include puppet
+  include sudo
+
+  include release
+
+  include munin::readonly
+  include munin-node
+
+  include ntp::readonly
+}
+
+class box::audio {
+  include alsa::common
+  include alsa::readonly
+  include alsa::mixer
+}
+
+class box::storage {
+  include mdadm
+  # TODO completed with PigeBox/StageBox/ChouetteBox 
+}
