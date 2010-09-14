747137c1010eb49b2b50ebe4b4cc191961721e07
Alban Peignier <alban.peignier@free.fr>
2010-09-14 23:02:00 +0200

    Import ssh configuration from StreamBox project. Refs #8

---
+++ manifests/classes/ssh.pp
@@ -0,0 +1,12 @@
+class ssh {
+  package { ssh: }
+
+  file { 
+    "/root/.ssh/authorized_keys":
+    source => "puppet:///box/ssh/authorized_keys",
+    mode => 700;
+
+    "/root/.ssh": 
+    ensure => directory, mode => 700;
+  }
+}
