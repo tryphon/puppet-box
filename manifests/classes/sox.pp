88ff23c06c8895a956e4b4ff6dddda3dce2c33ee
Alban Peignier <alban@tryphon.eu>
2011-01-14 18:38:37 +0100

    Add sox provided by debian.tryphon.eu with lame support

--- manifests/classes/sox.pp
+++ manifests/classes/sox.pp
@@ -1,3 +1,9 @@
 class sox {
-  package { [sox, libsox-fmt-all]: }
+  include apt::tryphon
+  include apt::multimedia # required for lame
+
+  package { [sox, libsox-fmt-mp3]: 
+    ensure => "14.3.1-1~bpo50+1",
+    require => [Apt::Source[tryphon], Apt::Source[debian-multimedia]] 
+  }
 }
