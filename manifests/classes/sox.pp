6b0699f92e6a4fd0993478b83a24d5d328ff8ea4
Alban Peignier <alban@tryphon.eu>
2011-03-03 23:10:57 +0100

    Use new sox rebuild to use libmp3lame (instead of liblame)

--- manifests/classes/sox.pp
+++ manifests/classes/sox.pp
@@ -3,7 +3,7 @@ class sox {
   include apt::multimedia # required for lame
 
   package { [sox, libsox-fmt-mp3]: 
-    ensure => "14.3.1-1~bpo50+1",
+    ensure => "14.3.1-1~bpo50+2",
     require => [Apt::Source[tryphon], Apt::Source[debian-multimedia]] 
   }
 }
