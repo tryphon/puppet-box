2074bb2d36bc98d829a25fe76955a3baf3a22702
Alban Peignier <alban@tryphon.eu>
2011-05-19 19:11:43 +0200

    Use optionnal release_cron_after_download and release_cron_download_directory variables in release::cron

--- manifests/classes/release.pp
+++ manifests/classes/release.pp
@@ -32,9 +32,18 @@ class release::cron {
     '' => "/bin/true",
     default => $release_cron_before_download
   }
+  $real_release_cron_after_download = $release_cron_after_download ? {
+    '' => "/bin/true",
+    default => $release_cron_after_download
+  }
+  $download_directory_option = $release_cron_download_directory ? {
+    '' => '',
+    default => "--download-dir=$release_cron_download_directory"
+  }
+  $cron_command = "/var/lib/gems/1.8/bin/box-release --current /boot/current.yml --install /usr/local/sbin/box-upgrade --before-download='$real_release_cron_before_download' --after-download='$real_release_cron_after_download' $download_directory_option http://download.tryphon.eu/$box_name/latest.yml"
 
   file { "/etc/cron.d/box-release":
-    content => "30 *    * * *   root	/var/lib/gems/1.8/bin/box-release --current /boot/current.yml --install /usr/local/sbin/box-upgrade --before-download='$real_release_cron_before_download' http://download.tryphon.eu/$box_name/latest.yml\n",
+    content => "30 *    * * *   root	$cron_command\n",
     require => Package[box-release]
   }
 }
