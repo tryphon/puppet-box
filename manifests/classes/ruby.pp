44cdda9ee2bda62c6c608e442e7198aeed64f6da
Alban Peignier <alban@tryphon.eu>
2011-05-20 21:08:37 +0200

    Use tryphon lenny-backports for rubygems

--- manifests/classes/ruby.pp
+++ manifests/classes/ruby.pp
@@ -1,10 +1,11 @@
 class ruby::gems {
-  include apt::backport
+  include apt::tryphon
 
   package { rubygems: 
     require => [Apt::Source::Pin[rubygems], Apt::Source::Pin["rubygems1.8"]]
   }
   apt::source::pin { [rubygems, "rubygems1.8"]:
-    source => "lenny-backports"
+    source => "lenny-backports",
+    require => Apt::Source[tryphon]
   }
 }
