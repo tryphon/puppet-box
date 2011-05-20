b72d12e54f0de698bb120807bb169d46f23c42dd
Alban Peignier <alban@tryphon.eu>
2011-05-20 18:01:27 +0200

    Use both rubygems and rubygems1.8 from lenny-backports

--- manifests/classes/ruby.pp
+++ manifests/classes/ruby.pp
@@ -2,9 +2,9 @@ class ruby::gems {
   include apt::backport
 
   package { rubygems: 
-    require => Apt::Source::Pin[rubygems]
+    require => [Apt::Source::Pin[rubygems], Apt::Source::Pin["rubygems1.8"]]
   }
-  apt::source::pin { rubygems:
+  apt::source::pin { [rubygems, "rubygems1.8"]:
     source => "lenny-backports"
   }
 }
