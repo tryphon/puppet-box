9e2a5d6f8f4dedc929172aedb6fdb459f56e118a
Alban Peignier <alban@tryphon.eu>
2012-01-22 16:54:29 +0100

    Ensure rubygems is backport release on squeeze

--- manifests/classes/ruby.pp
+++ manifests/classes/ruby.pp
@@ -1,10 +1,12 @@
 class ruby::gems {
   file { "/etc/gemrc":
-    content => "gem: --no-rdoc --no-ri\n"
+    content => "gem: --no-rdoc --no-ri\n",
+    before => Package[rubygems]
   }
   if $debian::lenny {
     include apt::tryphon
     package { rubygems: 
+      ensure => "1.3.7-1~bpo50+1",
       require => [Apt::Source::Pin[rubygems], Apt::Source::Pin["rubygems1.8"]]
     }
     apt::source::pin { [rubygems, "rubygems1.8"]:
