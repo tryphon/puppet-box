cc44849e6a3ddce07eae89fceb2a9b2f144ae6aa
Alban Peignier <alban@tryphon.eu>
2012-10-29 00:04:41 +0100

    Change /etc/init.d/mdadm-raid to add --no-degraded option for mdadm assembling. Refs #396

--- manifests/classes/mdadm.pp
+++ manifests/classes/mdadm.pp
@@ -1,3 +1,10 @@
 class mdadm {
   package { mdadm: }
+
+  if $debian::squeeze {
+    file { "/etc/init.d/mdadm-raid":
+      source => "puppet:///box/mdadm/mdadm-raid.init.squeeze",
+      require => Package[mdadm]
+    }
+  }
 }
