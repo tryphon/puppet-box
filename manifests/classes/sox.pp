b86da3aab971461c4f2625edd8c32ca7c347ef70
Alban Peignier <alban@tryphon.eu>
2013-02-11 12:45:09 +0100

    Use sox 14.4.0-3~bpo60+2 on squeeze

--- manifests/classes/sox.pp
+++ manifests/classes/sox.pp
@@ -2,8 +2,14 @@ class sox {
   include apt::tryphon
   include apt::multimedia # required for lame
 
+  $version = $debian::release ? {
+    lenny => "14.3.1-1~bpo50+2",
+    squeeze => "14.4.0-3~bpo60+2",
+    default => "latest"
+  }
+
   package { [sox, libsox-fmt-mp3]: 
-    ensure => "14.3.1-1~bpo50+2",
+    ensure => $version,
     require => [Apt::Source[tryphon], Apt::Source[debian-multimedia]] 
   }
 }
