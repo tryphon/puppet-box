c6e145b2456c6a0c6d40215a5739c9e1f5d5a071
Alban Peignier <alban@tryphon.eu>
2013-05-11 15:36:46 +0200

    Remove all exim4 packages

--- manifests/classes/smtp.pp
+++ manifests/classes/smtp.pp
@@ -1,4 +1,6 @@
 class smtp {
   # disable exim4 installed by default
-  package { "exim4-daemon-light": ensure => purged }
+  package { ["exim4-daemon-light", "exim4", "exim4-base", "exim4-config", "exim4-daemon-heavy"]: 
+    ensure => purged 
+  }
 }
