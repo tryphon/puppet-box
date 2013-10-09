01fb79c8a4d22cfc3761637f852775a281a657d0
Alban Peignier <alban@tryphon.eu>
2013-10-09 19:40:14 +0200

    Use box gem 0.0.14 (to use new Box.setup)

--- manifests/classes/box.pp
+++ manifests/classes/box.pp
@@ -51,7 +51,7 @@ define box::config::migration($source) {
 class box::gem {
   file { "/etc/box": ensure => directory }
 
-  ruby::gem { tryphon-box: ensure => "0.0.13" }
+  ruby::gem { tryphon-box: ensure => "0.0.14" }
   ruby::gem { SyslogLogger: ensure => "2.0" }
 
   file { "/etc/cron.d/box":
