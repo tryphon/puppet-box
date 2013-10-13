1afb7de6ba3ab430dfbf3869ed1cd1c2b99e0f65
Alban Peignier <alban@tryphon.eu>
2013-10-13 17:02:14 +0200

    Use httparty 0.11.0 to avoid problem with ruby 1.8

--- manifests/classes/box.pp
+++ manifests/classes/box.pp
@@ -51,8 +51,9 @@ define box::config::migration($source) {
 class box::gem {
   file { "/etc/box": ensure => directory }
 
-  ruby::gem { tryphon-box: ensure => "0.0.15" }
+  ruby::gem { tryphon-box: ensure => "0.0.15", require => Ruby::Gem[httparty] }
   ruby::gem { SyslogLogger: ensure => "2.0" }
+  ruby::gem { httparty: ensure => "0.11.0" }
 
   file { "/etc/cron.d/box":
     content => "*/5 *    * * *   root	/usr/local/bin/random-sleep 50 5 && /usr/local/bin/box provisioning sync\n",
