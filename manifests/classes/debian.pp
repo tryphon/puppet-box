8c0167a77efda5d38ece6c94d3b0f5744e638c86
Alban Peignier <alban@tryphon.eu>
2014-04-29 22:37:47 +0200

    Improve debian class to detect wheezy. Refs #1044

--- manifests/classes/debian.pp
+++ manifests/classes/debian.pp
@@ -1,17 +1,30 @@
 class debian {
-  if $operatingsystem == "Debian" and versioncmp($operatingsystemrelease, "6.0") >= 0 {
-    $release = "squeeze"
+  if $operatingsystem == "Debian" {
+    if versioncmp($operatingsystemrelease, "7.0") >= 0 {
+      $release = "wheezy"
+    } elsif versioncmp($operatingsystemrelease, "6.0") >= 0 {
+      $release = "squeeze"
+    } elsif versioncmp($operatingsystemrelease, "5.0") >= 0 {
+      $release = "lenny"
+    } else {
+      fail("Can't detect debian release")
+    }
   } else {
-    $release = "lenny"
+    $release = "none"
   }
-  
+
   $lenny = $release ? {
     "lenny" => true,
     default => false
   }
-  
+
   $squeeze = $release ? {
     "squeeze" => true,
+    default => false
+  }
+
+  $wheezy = $release ? {
+    "wheezy" => true,
     default => false
   }
 }
