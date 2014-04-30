a3d7e4c08f08d029212671cd29941adcd3889a40
Alban Peignier <alban@tryphon.eu>
2014-04-30 23:12:35 +0200

    Fixes lint warnings in reverse-tunnel class

--- manifests/classes/reverse-tunnel.pp
+++ manifests/classes/reverse-tunnel.pp
@@ -1,25 +1,26 @@
 class reverse-tunnel {
-  ruby::gem { reverse-tunnel: }
+  ruby::gem { 'reverse-tunnel': }
 
-  file { "/etc/init.d/reverse-tunnel":
-    source => "puppet:///box/reverse-tunnel/reverse-tunnel.init",
-    require => File["/etc/default/reverse-tunnel"],
-    mode => 775
+  file { '/etc/init.d/reverse-tunnel':
+    source  => 'puppet:///box/reverse-tunnel/reverse-tunnel.init',
+    require => File['/etc/default/reverse-tunnel'],
+    mode    => '0775'
   }
 
-  exec { "update-rc.d reverse-tunnel defaults":
-    require => File["/etc/init.d/reverse-tunnel"],
-    creates => "/etc/rc0.d/K20reverse-tunnel"
+  exec { 'update-rc.d reverse-tunnel defaults':
+    require => File['/etc/init.d/reverse-tunnel'],
+    creates => '/etc/rc0.d/K20reverse-tunnel'
   }
 
-  file { "/etc/puppet/templates/reverse-tunnel.default":
-    source => "puppet:///box/reverse-tunnel/reverse-tunnel.default.erb"
+  file { '/etc/puppet/templates/reverse-tunnel.default':
+    source => 'puppet:///box/reverse-tunnel/reverse-tunnel.default.erb'
   }
-  file { "/etc/puppet/manifests/classes/reverse-tunnel.pp":
-    source => "puppet:///box/reverse-tunnel/manifest.pp"
+  file { '/etc/puppet/manifests/classes/reverse-tunnel.pp':
+    source => 'puppet:///box/reverse-tunnel/manifest.pp'
   }
 
-  file { "/etc/default/reverse-tunnel":
-    ensure => "/var/etc/default/reverse-tunnel"
+  file { '/etc/default/reverse-tunnel':
+    ensure => link,
+    target => '/var/etc/default/reverse-tunnel'
   }
 }
