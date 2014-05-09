40518dfd1588af49319b9a469854e449a1846cbe
Alban Peignier <alban@tryphon.eu>
2014-05-09 10:19:15 +0200

    Provides cucumber steps to manage Box configuration

---
+++ features/step_definitions/box_configuration_steps.rb
@@ -0,0 +1,23 @@
+puts "meuh"
+
+Given /^the box configuration contains ([a-z_]+) = "([^"]*)"$/ do |key, value|
+  current_box.configuration do |config|
+    config[key] = value
+    config.save
+  end
+end
+
+Given /^the box configuration doesn't contain ([a-z_]+)$/ do |key|
+  current_box.configuration do |config|
+    config.delete key
+    config.save
+  end
+end
+
+When /^the configuration is applied$/ do
+  current_box.configuration.deploy!
+end
+
+Given /^the box configuration is saved$/ do
+  current_box.configuration.persist!
+end
