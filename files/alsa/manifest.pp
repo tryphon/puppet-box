1b1cefd3d381b42bb21a6ea3b72c37afebc22cba
Alban Peignier <alban@tryphon.eu>
2014-06-01 19:31:39 +0200

    Define a alsa class (to manage dependencies correctly). Define default alsa::alsa_config when RME HDSP is detected. See #1087

--- files/alsa/manifest.pp
+++ files/alsa/manifest.pp
@@ -1,13 +1,26 @@
 $alsa_device = generate("/usr/local/bin/alsa-device")
 
-if $alsa_config != "" {
-  file { "/var/etc/asound.conf":
-    content => template("/etc/puppet/templates/alsa/asound.conf"),
-    tag => boot
+class alsa {
+  $device_id = inline_template("<%= File.read('/proc/asound/card0/id').chomp %>")
+
+  # OPTIMIZEME specific to RivendellBoxes (see #1087)
+  if $alsa_config == "" and $alsa::device_id == "DSP" {
+    notice("Define default alsa_config for RME HDSP support");
+    $alsa_config = { "pcm.rd0" => { "type" => "plug", "slave.pcm" => "hw:0" } }
   }
-} else {
-  file { "/var/etc/asound.conf":
-    source => "/etc/puppet/files/alsa/asound.conf.default",
-    tag => boot
+
+  if $alsa_config != "" {
+    file { "/var/etc/asound.conf":
+      content => template("/etc/puppet/templates/alsa/asound.conf"),
+      tag => boot
+    }
+  } else {
+    file { "/var/etc/asound.conf":
+      source => "/etc/puppet/files/alsa/asound.conf.default",
+      tag => boot
+    }
   }
+
 }
+
+include alsa
