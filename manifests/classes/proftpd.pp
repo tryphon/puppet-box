1a52994548ef17d91dc4a5a374d38a6c86878371
Alban Peignier <alban@tryphon.eu>
2014-07-25 20:11:58 +0200

    Add user and group ftp

--- manifests/classes/proftpd.pp
+++ manifests/classes/proftpd.pp
@@ -1,5 +1,16 @@
 class proftpd::steto {
-  steto::conf { proftpd: 
+  steto::conf { proftpd:
     source => "puppet:///box/proftpd/steto.rb"
+  }
+}
+
+class proftpd::common {
+  user { ftp:
+    uid => 2050,
+    gid => 2050,
+    shell => '/bin/sh'
+  }
+  group { ftp:
+    gid => 2050
   }
 }
