ce22027b65702c5232d04c3184caa8393f242d09
Alban Peignier <alban@tryphon.eu>
2014-07-25 20:12:26 +0200

    Update go-broadcast to 0.11+build81

--- manifests/classes/go-broadcast.pp
+++ manifests/classes/go-broadcast.pp
@@ -1,6 +1,7 @@
 class go-broadcast {
+  include apt::tryphon::dev
   package { "go-broadcast":
-    ensure => "0.10",
-    require => Apt::Source[tryphon]
+    ensure => "0.11+build81",
+    require => Apt::Source[tryphon-dev]
   }
 }
