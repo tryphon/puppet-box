de84420b8aa79db5757a29312f9ad75bc6cc3952
Alban Peignier <alban@tryphon.eu>
2014-08-30 14:44:50 +0200

    Update go-broadcast. Add munin graph which uses SoundMeter history

--- manifests/classes/go-broadcast.pp
+++ manifests/classes/go-broadcast.pp
@@ -1,7 +1,18 @@
 class go-broadcast {
   include apt::tryphon::dev
+  # require for lame backport
+  include apt::tryphon
   package { "go-broadcast":
-    ensure => "0.11+build81",
-    require => Apt::Source[tryphon-dev]
+    ensure => "0.11+build99",
+    require => [Apt::Source[tryphon-dev], Apt::Source[tryphon]]
+  }
+  include go-broadcast::munin
+}
+
+class go-broadcast::munin {
+  include munin::ruby
+
+  munin::plugin { gobroadcast_soundmeter:
+    source => "puppet:///box/go-broadcast/munin/soundmeter"
   }
 }
