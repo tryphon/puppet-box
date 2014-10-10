d8e4cbe7d2959f11714ef26a98294de95177a53d
Alban Peignier <alban@tryphon.eu>
2014-10-10 16:12:33 +0200

    Use go-broadcast 0.11+build101 (available for i386)

--- manifests/classes/go-broadcast.pp
+++ manifests/classes/go-broadcast.pp
@@ -3,7 +3,7 @@ class go-broadcast {
   include apt::multimedia # required for lame
 
   package { "go-broadcast":
-    ensure => "0.11+build99",
+    ensure => "0.11+build101",
     require => [Apt::Source[tryphon-dev], Apt::Source[debian-multimedia]]
   }
   include go-broadcast::munin
