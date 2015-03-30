072124548763eddd03c00528f304270e541a6476
Alban Peignier <alban@tryphon.eu>
2015-03-30 23:38:15 +0200

    Use box.type.latest_url in steto :box_release_latest_http. Fixes #1367

--- files/release/steto.rb
+++ files/release/steto.rb
@@ -1,15 +1,7 @@
 Steto.config do
 
   check :box_release_latest_http do
-    url = case box.name
-          when "rivendellnasbox" 
-            "/rivendellboxes/latest.yml"
-          else 
-            "/#{box.name}/latest.yml"
-          end
-    
-    nagios_check = Steto::NagiosCheck.new :command => "/usr/lib/nagios/plugins/check_http", :options => { :host => "download.tryphon.eu", :url => url }
-
+    nagios_check = Steto::NagiosCheck.new :command => "/usr/lib/nagios/plugins/check_http", :options => { :host => "download.tryphon.eu", :url => box.type.latest_url }
     # only returns a warning
     nagios_check.status.critical? ? :warning : nagios_check.status
   end
