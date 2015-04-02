f31b2ae3ccf737be9b274be6e608eb01798bcbd5
Alban Peignier <alban@tryphon.eu>
2015-04-02 15:56:57 +0200

    Add CreateGoBroadcastStreams migration. Refs #1370

---
+++ files/go-broadcast/migrations/20150401085239_create_go_broadcast_streams.rb
@@ -0,0 +1,54 @@
+class CreateGoBroadcastStreams < Box::Config::Migration
+
+  @@gobroadcast_config_file = "/boot/data/go-broadcast.json"
+  cattr_accessor :gobroadcast_config_file
+
+  def gobroadcast_config
+    JSON.parse File.read(gobroadcast_config_file) rescue {}
+  end
+
+  def up
+    return unless gobroadcast_config.empty?
+
+    gobroadcast_streams = []
+
+    (1..4).each do |stream_id|
+      attributes = config.attributes("stream_#{stream_id}")
+      unless attributes.empty?
+        gobroadcast_streams << create_stream(attributes)
+      end
+    end
+
+    File.write gobroadcast_config_file, { "Http" => { "Streams" => gobroadcast_streams } }.to_json
+  end
+
+  def create_stream(attributes = {})
+    format = [].tap do |parts|
+      parts << (attributes[:format] == "vorbis" ? "ogg/vorbis" : attributes[:format])
+
+      mode_attributes = {
+        b: attributes[:bitrate],
+        q: attributes[:quality]
+      }.map { |k,v| "#{k}=#{v}" if v.present? }.compact.join(',')
+      mode_attributes = "(#{mode_attributes})" if mode_attributes.present?
+
+      parts << "#{attributes[:mode]}#{mode_attributes}"
+    end.join(':')
+
+    {
+      "Identifier" => attributes[:id],
+      "Target" => "http://source:#{attributes[:password]}@#{attributes[:server]}:#{attributes[:port]}/#{attributes[:mount_point]}",
+      "Format" => format
+    }.tap do |stream|
+      description = {
+        "Name" => attributes[:name],
+		    "Description" => attributes[:description],
+		    "URL" => attributes[:related_url],
+		    "Genre" => attributes[:genre]
+      }
+      description.delete_if { |_,v| v.blank? }
+      stream["Description"] = description unless description.empty?
+    end
+  end
+
+end
---
+++ spec/gobroadcast_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe "GoBroadcast" do
+
+  describe "migration CreateGoBroadcastStreams" do
+
+    let(:config) { Box::PuppetConfiguration.new }
+    let(:migration) { Box::Config::Migration.create File.expand_path("../../files/go-broadcast/migrations/20150401085239_create_go_broadcast_streams.rb", __FILE__) }
+
+    let(:gobroadcast_config_file) { "tmp/go-broadcast.json" }
+    let(:gobroadcast_config) { JSON.parse File.read(gobroadcast_config_file) }
+
+    before do
+      FileUtils.rm_f gobroadcast_config_file
+
+      migration.gobroadcast_config_file = gobroadcast_config_file
+      migration.config = config
+    end
+
+    it "should create GoBroadcast streams configuration with stream_... attributes" do
+      config[:stream_1_id]="1"
+      config[:stream_1_server]="stream-in.tryphon.eu"
+      config[:stream_1_port]="8000"
+      config[:stream_1_mount_point]="streambox.ogg"
+      config[:stream_1_format]="vorbis"
+      config[:stream_1_mode]="vbr"
+      config[:stream_1_quality]="4"
+      config[:stream_1_password]="secret"
+
+      migration.up
+
+      gobroadcast_config["Http"]["Streams"][0]["Identifier"].should == "1"
+      gobroadcast_config["Http"]["Streams"][0]["Target"].should == "http://source:secret@stream-in.tryphon.eu:8000/streambox.ogg"
+      gobroadcast_config["Http"]["Streams"][0]["Format"].should == "ogg/vorbis:vbr(q=4)"
+    end
+
+    describe "#create_stream" do
+
+      it "should use id as Identifier" do
+        migration.create_stream(id: "dummy")["Identifier"].should == "dummy"
+      end
+
+      it "should create Target from password, server, port, mount_point" do
+        migration.create_stream(password: "secret", secret: "stream-in.tryphon.eu", port: "8000", mount_point: "dummy")["Target"] == "http://source:secret@stream-in.tryphon.eu:8000/dummy"
+      end
+
+      it "should create Format from quality, bitrate and format attributes" do
+        migration.create_stream(format: "vorbis", mode: "vbr", quality: "5")["Format"].should == "ogg/vorbis:vbr(q=5)"
+        migration.create_stream(format: "mp3", mode: "cbr", bitrate: "160")["Format"].should == "mp3:cbr(b=160)"
+        migration.create_stream(format: "aac", mode: "vbr")["Format"].should == "aac:vbr"
+      end
+
+      it "should not include Description when empty" do
+        migration.create_stream["Description"].should be_nil
+      end
+
+      it "should include Description with present attributes" do
+        migration.create_stream({ genre: "", name: "dummy"  })["Description"].should == { "Name" => "dummy" }
+      end
+
+    end
+
+  end
+
+end
